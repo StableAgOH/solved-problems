b4b796d72f68612e5e64dff8a80fad609140f1f0
AgOH <stagoh@qq.com>
2022-04-18 13:20:14 +0800

    add(nowcoder): contest 11199

---
+++ nowcoder/contest/11199/A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+//==========================================
+typedef long long ll;
+signed main(signed argc, char const *argv[])
+{
+#ifdef LOCAL
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    auto c1 = chrono::high_resolution_clock::now();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //======================================
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        ll l = a/2;
+        ll r = a/2+a%2;
+        cout<<l*r*b-l*b<<endl;
+    }
+    //======================================
+#ifdef LOCAL
+    auto c2 = chrono::high_resolution_clock::now();
+    cerr<<"Time Used:"<<chrono::duration_cast<chrono::milliseconds>(c2-c1).count()<<"ms"<<endl;
+#endif
+    return 0;
+}
