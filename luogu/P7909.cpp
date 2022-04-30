1e95f3192708a9c4d87ba344ef82f336f13ade4a
AgOH <stagoh@qq.com>
2022-04-30 20:30:35 +0800

    add(luogu): P7909

---
+++ luogu/P7909.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+//==========================================
+const int maxn = 1e5+5;
+signed main(signed argc, char const *argv[])
+{
+#ifdef LOCAL
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    auto c1 = chrono::high_resolution_clock::now();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    //======================================
+    int n,l,r;
+    cin>>n>>l>>r;
+    cout<<min(r,l+(n-1-l%n))%n<<endl;
+    //======================================
+#ifdef LOCAL
+    auto c2 = chrono::high_resolution_clock::now();
+    cerr<<"Time Used:"<<chrono::duration_cast<chrono::milliseconds>(c2-c1).count()<<"ms"<<endl;
+#endif
+    return 0;
+}
